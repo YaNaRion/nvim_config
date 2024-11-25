081d29331e541ffef39f097b51603235ae4da796
YaNaRion <druet.goldorak@gmail.com>
2024-11-25 17:48:43 -0500

    fix du bug de init du theme

--- lua/custom/plugins/theme.lua
+++ lua/custom/plugins/theme.lua
@@ -1,4 +1,3 @@
-vim.cmd.colorscheme 'default'
 return {
   {
     'olivercederborg/poimandres.nvim',
@@ -8,5 +7,14 @@ return {
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
     'folke/tokyonight.nvim',
     priority = 1000,
+    init = function()
+      -- Load the colorscheme here.
+      -- Like many other themes, this one has different styles, and you could load
+      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+      vim.cmd.colorscheme 'tokyonight-night'
+
+      -- You can configure highlights by doing something like:
+      vim.cmd.hi 'Comment gui=none'
+    end,
   },
 }
