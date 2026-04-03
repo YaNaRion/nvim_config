6f50871cbcc10dd3b4754d3d99c431ed7ef9efd8
YaNaRion <druet.goldorak@gmail.com>
2026-04-03 00:58:59 -0400

    clean unuse theme et fix color bug

--- init.lua
+++ init.lua
@@ -2,12 +2,13 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 -- vim.opt.termguicolors = true
+--
 
 vim.opt.termguicolors = true
 -- vim.cmd 'colorscheme base16-default-dark'
 vim.opt.wildignore:append { '**/.git/**', '**/node_modules/**', '**/build/**', '*.o', '*.pyc', '*.swp' }
 
-vim.g.have_nerd_font = truek
+vim.g.have_nerd_font = true
 vim.g.shiftwidth = 4
 vim.opt.guicursor = 'n-v-i-c:block-Cursor'
 
@@ -91,84 +92,83 @@ vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper win
 vim.keymap.set('n', '<C-d>', '<C-d>zz')
 vim.keymap.set('n', '<C-u>', '<C-u>zz')
 
-if vim.g.vscode then
-else
-  -- [[ Basic Autocommands ]]
-  -- Highlight when yanking (copying) text
-  vim.api.nvim_create_autocmd('TextYankPost', {
-    desc = 'Highlight when yanking (copying) text',
-    group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-    callback = function()
-      vim.highlight.on_yank()
-    end,
-  })
+-- [[ Basic Autocommands ]]
+-- Highlight when yanking (copying) text
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
 
-  -- [[ Install `lazy.nvim` plugin manager ]]
-  local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
-  if not (vim.uv or vim.loop).fs_stat(lazypath) then
-    local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
-    local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
-    if vim.v.shell_error ~= 0 then
-      error('Error cloning lazy.nvim:\n' .. out)
-    end
-  end ---@diagnostic disable-next-line: undefined-field
-  vim.opt.rtp:prepend(lazypath)
+-- [[ Install `lazy.nvim` plugin manager ]]
+local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
+  local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
+  if vim.v.shell_error ~= 0 then
+    error('Error cloning lazy.nvim:\n' .. out)
+  end
+end ---@diagnostic disable-next-line: undefined-field
+vim.opt.rtp:prepend(lazypath)
 
-  -- [[ Configure and install plugins ]]
-  require('lazy').setup({
-    'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
-    { -- Adds git related signs to the gutter, as well as utilities for managing changes
-      'lewis6991/gitsigns.nvim',
-      opts = {
-        signs = {
-          add = { text = '󱌃' },
-          change = { text = '' },
-          delete = { text = '󰳭' },
-          topdelete = { text = '󰳭' },
-          changedelete = { text = '' },
-        },
+-- [[ Configure and install plugins ]]
+require('lazy').setup({
+  'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
+  { -- Adds git related signs to the gutter, as well as utilities for managing changes
+    'lewis6991/gitsigns.nvim',
+    opts = {
+      signs = {
+        add = { text = '󱌃' },
+        change = { text = '' },
+        delete = { text = '󰳭' },
+        topdelete = { text = '󰳭' },
+        changedelete = { text = '' },
+      },
 
-        current_line_blame = true,
-        current_line_blame_formatter = '<author>, <author_time:%R> - <summary>',
-        current_line_blame_opts = {
-          delay = 1000,
-          ignore_whitespace = false,
-          virt_text = true,
-          virt_text_priority = 100,
-          virt_text_pos = 'eol',
-        },
+      current_line_blame = true,
+      current_line_blame_formatter = '<author>, <author_time:%R> - <summary>',
+      current_line_blame_opts = {
+        delay = 1000,
+        ignore_whitespace = false,
+        virt_text = true,
+        virt_text_priority = 100,
+        virt_text_pos = 'eol',
       },
     },
+  },
 
-    require 'kickstart.plugins.debug',
-    -- require 'kickstart.plugins.indent_line', si active probleme au lancement de nvim
-    require 'kickstart.plugins.lint',
-    -- require 'kickstart.plugins.autopairs',
-    -- require 'kickstart.plugins.neo-tree',
-    require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
+  require 'kickstart.plugins.debug',
+  -- require 'kickstart.plugins.indent_line', si active probleme au lancement de nvim
+  require 'kickstart.plugins.lint',
+  -- require 'kickstart.plugins.autopairs',
+  -- require 'kickstart.plugins.neo-tree',
+  require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
 
-    { import = 'plugins' },
-  }, {
-    ui = {
-      icons = vim.g.have_nerd_font and {} or {
-        cmd = '⌘',
-        config = '🛠',
-        event = '📅',
-        ft = '📂',
-        init = '⚙',
-        keys = '🗝',
-        plugin = '🔌',
-        runtime = '💻',
-        require = '🌙',
-        source = '📄',
-        start = '🚀',
-        task = '📌',
-        lazy = '💤 ',
-      },
+  { import = 'plugins' },
+}, {
+  ui = {
+    icons = vim.g.have_nerd_font and {} or {
+      cmd = '⌘',
+      config = '🛠',
+      event = '📅',
+      ft = '📂',
+      init = '⚙',
+      keys = '🗝',
+      plugin = '🔌',
+      runtime = '💻',
+      require = '🌙',
+      source = '📄',
+      start = '🚀',
+      task = '📌',
+      lazy = '💤 ',
     },
-  })
+  },
+})
 
-  -- The line beneath this is called `modeline`. See `:help modeline`
-  -- vim: ts=2 sts=2 sw=2
-  --
-end
+ColorMyPencils()
+
+-- The line beneath this is called `modeline`. See `:help modeline`
+-- vim: ts=2 sts=2 sw=2
+--
--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -9,19 +9,19 @@ function ColorMyPencils(colorFunc)
     vim.cmd.colorscheme(color)
   end
 
-  vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
-  vim.api.nvim_set_hl(0, 'NormalNC', { bg = 'none' })
-  vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
-  vim.api.nvim_set_hl(0, 'FloatBorder', { bg = 'none' })
-  vim.api.nvim_set_hl(0, 'SignColumn', { bg = 'none' })
-  vim.api.nvim_set_hl(0, 'VertSplit', { bg = 'none' })
-  vim.api.nvim_set_hl(0, 'StatusLine', { bg = 'none' })
-  vim.api.nvim_set_hl(0, 'TabLineFill', { bg = 'none' })
-  vim.api.nvim_set_hl(0, 'EndOfBuffer', { bg = 'none', fg = 'none' })
-  vim.api.nvim_set_hl(0, 'LineNr', { bg = 'none' })
-  vim.api.nvim_set_hl(0, 'CursorLineNr', { bg = 'none' })
-  vim.api.nvim_set_hl(0, 'LineNrAbove', { bg = 'none' })
-  vim.api.nvim_set_hl(0, 'LineNrBelow', { bg = 'none' })
+  -- vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
+  -- vim.api.nvim_set_hl(0, 'NormalNC', { bg = 'none' })
+  -- vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
+  -- vim.api.nvim_set_hl(0, 'FloatBorder', { bg = 'none' })
+  -- vim.api.nvim_set_hl(0, 'SignColumn', { bg = 'none' })
+  -- vim.api.nvim_set_hl(0, 'VertSplit', { bg = 'none' })
+  -- vim.api.nvim_set_hl(0, 'StatusLine', { bg = 'none' })
+  -- vim.api.nvim_set_hl(0, 'TabLineFill', { bg = 'none' })
+  -- vim.api.nvim_set_hl(0, 'EndOfBuffer', { bg = 'none', fg = 'none' })
+  -- vim.api.nvim_set_hl(0, 'LineNr', { bg = 'none' })
+  -- vim.api.nvim_set_hl(0, 'CursorLineNr', { bg = 'none' })
+  -- vim.api.nvim_set_hl(0, 'LineNrAbove', { bg = 'none' })
+  -- vim.api.nvim_set_hl(0, 'LineNrBelow', { bg = 'none' })
 end
 
 return {
@@ -35,31 +35,6 @@ return {
     },
   },
   {
-    'olivercederborg/poimandres.nvim',
-    priority = 1000,
-  },
-  {
-    'folke/tokyonight.nvim',
-    priority = 1000,
-    transparent = true,
-    init = function()
-      ColorMyPencils(color)
-    end,
-  },
-  {
-    'FrenzyExists/aquarium-vim',
-    priority = 1000,
-  },
-  {
-    'sainnhe/everforest',
-    priority = 1000,
-    config = function()
-      vim.g.everforest_enable_italic = true
-      vim.g.everforest_background = 'hard'
-      vim.g.everforest_ui_contrast = 'high'
-    end,
-  },
-  {
     'rose-pine/neovim',
     styles = {
       bold = true,
@@ -69,23 +44,9 @@ return {
     priority = 1000,
   },
   {
-    'ficcdaf/ashen.nvim',
-    transparent = true,
-    priority = 1000,
-  },
-  {
-    'Mofiqul/vscode.nvim',
-    priority = 1000,
-  },
-  {
     'rockyzhang24/arctic.nvim',
     branch = 'v2',
     dependencies = { 'rktjmp/lush.nvim' },
-  },
-  {
-    'navarasu/onedark.nvim',
-    style = 'Darker',
-    transparent = true,
   },
 
   {
@@ -179,27 +140,5 @@ return {
         },
       }
     end,
-  },
-  { 'catppuccin/nvim', name = 'catppuccin', priority = 1000 },
-  { 'diegoulloao/neofusion.nvim', priority = 1000, config = true, opts = ... },
-  {
-    url = 'https://codeberg.org/jthvai/lavender.nvim',
-    branch = 'stable', -- versioned tags + docs updates from main
-    lazy = false,
-    priority = 1000,
-  },
-  {
-    'zenbones-theme/zenbones.nvim',
-    -- Optionally install Lush. Allows for more configuration or extending the colorscheme
-    -- If you don't want to install lush, make sure to set g:zenbones_compat = 1
-    -- In Vim, compat mode is turned on as Lush only works in Neovim.
-    dependencies = 'rktjmp/lush.nvim',
-    lazy = false,
-    priority = 1000,
-    -- you can set set configuration options here
-    -- config = function()
-    --     vim.g.zenbones_darken_comments = 45
-    --     vim.cmd.colorscheme('zenbones')
-    -- end
   },
 }
